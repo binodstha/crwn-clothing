3e8f4f7e4e6055bbda1fc9be55674be0673f9ff0
binod <ekbana@ekbanas-Mac-mini.local>
2020-08-29 23:41:00 +0545

    upgraded in app page is redirected to home if user is sign in:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-
 import { connect } from 'react-redux';
-
-
-
-import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -15,6 +9,7 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { setCurrentUser } from './redux/user/user.actions';
 
+import './App.css';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -48,7 +43,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />  
+          <Route exact path="/signin" render={() => this.props.currentUser ? <Redirect to="/" /> : (<SignInAndSignUp />)} />  
         </Switch>
       </div>
     );
@@ -56,8 +51,15 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 }) 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
